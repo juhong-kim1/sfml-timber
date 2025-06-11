2a947ac412fb3599cd8eb87568f6441d02848902
김주홍 <gicente1@nate.com>
2025-06-11 18:40:00 +0900

    25.06.11 end of class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <ctime>
+#include <cstdlib>
+
+
+
 
 int main()
 {
+    srand((int)time(0));
+
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Timber!");
 
     sf::Texture textureBackground;
@@ -18,26 +25,90 @@ int main()
     spriteBackground.setTexture(textureBackground);
     sf::Sprite spriteTree;
     spriteTree.setTexture(textureTree);
-    sf::Sprite spriteCloud;
-    spriteCloud.setTexture(textureCloud);
-    sf::Sprite spriteCloud2;
-    spriteCloud2.setTexture(textureCloud);
-    sf::Sprite spriteCloud3;
-    spriteCloud3.setTexture(textureCloud);
     sf::Sprite spriteBee;
     spriteBee.setTexture(textureBee);
 
 
+    sf::Sprite spriteCloud[3];
 
-    spriteTree.setPosition(1920 * 0.5f - textureTree.getSize().x *0.5f, 0);
-    spriteCloud2.setPosition(0, 0 + textureCloud.getSize().y);
-    spriteCloud3.setPosition(0, 0 + textureCloud.getSize().y * 2);
-    spriteBee.setPosition(0, 750);
+    for (int i = 0;i < 3; i++)
+    {
+        spriteCloud[i].setTexture(textureCloud);
+    }
+
+    float cloudY[3];
+
+    cloudY[0] = 0;
+    cloudY[1] = textureCloud.getSize().y;
+    cloudY[2] = textureCloud.getSize().y * 2;
 
 
+    spriteTree.setPosition(1920 * 0.5f - textureTree.getSize().x * 0.5f, 0);
+    spriteCloud[0].setPosition(960, 0);
+    spriteCloud[1].setPosition(960, 0 + textureCloud.getSize().y);
+    spriteCloud[2].setPosition(960, 0 + textureCloud.getSize().y * 2);
+    spriteBee.setPosition(960, 750);
+
+
+
+    sf::Vector2f beeDirection = { 1.f, 0.f };
+    float beeSpeed = 200.f;
+    float beerandom = (float)rand() / RAND_MAX;
+    if (beerandom > 0.5)
+    {
+        beeDirection.x = 1.f;
+        spriteBee.setScale(-1.f, 1.f);
+    }
+    else
+    {
+        beeDirection.x = -1.f;
+        spriteBee.setScale(1.f, 1.f);
+    }
+    beeSpeed = rand() % 200 + 100;
+
+    sf::Vector2f cloudDirection[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cloudDirection[i] = { 1.f, 0.f };
+    }
+
+    float cloudspeed[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cloudspeed[i] = 200.f;
+    }
+
+
+    for (int i = 0; i < 3; i++)
+    {
+  
+        float cloud1random = (float)rand() / RAND_MAX;
+        if (cloud1random > 0.5)
+        {
+            cloudDirection[i].x = 1.f;
+            spriteCloud[i].setScale(-1.f, 1.f);
+        }
+        else
+        {
+            cloudDirection[i].x = -1.f;
+            spriteCloud[i].setScale(1.f, 1.f);
+        }
+        cloudspeed[i] = rand() % 200 + 100;
+    }
+
+
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        sf::Time time = clock.restart();
+        float deltaTime = time.asSeconds();
+
+        printf("%f\n", deltaTime);
+
+
+        // 이벤트 루프
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -45,18 +116,71 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        // 업데이트
+        sf::Vector2f pos = spriteBee.getPosition();
+        pos += beeDirection * beeSpeed * deltaTime;
+        spriteBee.setPosition(pos);
+
+        for (int i = 0; i < 3; i++)
+        {
+            sf::Vector2f pos_C1 = spriteCloud[i].getPosition();
+            pos_C1 += cloudDirection[i] * cloudspeed[i] * deltaTime;
+            spriteCloud[i].setPosition(pos_C1);
+
+            if (pos_C1.x < -800 || pos_C1.x > 1920 + 800)
+            {
+                float cloud1random = (float)rand() / RAND_MAX;
+                if (cloud1random < 0.5f)
+                {
+                    cloudDirection[i].x = 1.f;
+                    spriteCloud[i].setScale(-1.f, 1.f);
+                    spriteCloud[i].setPosition(0 - 100, 0 + cloudY[i]);
+                   
+                }
+                else
+                {
+                    cloudDirection[i].x = -1.f;
+                    spriteCloud[i].setScale(1.f, 1.f);
+                    spriteCloud[i].setPosition(1920 + 100, 0 + cloudY[i]);
+                }
+                cloudspeed[i] = rand() % 200 + 100;
+
+            }
+        }
+
+            if (pos.x < -200 || pos.x > 1920 + 200)
+            {
+                float beerandom = (float)rand() / RAND_MAX;
+                if (beerandom < 0.5f)
+                {
+                    beeDirection.x = 1.f;
+                    spriteBee.setScale(-1.f, 1.f);
+                    spriteBee.setPosition(0 - 150, 750);
+                }
+                else
+                {
+                    beeDirection.x = -1.f;
+                    spriteBee.setScale(1.f, 1.f);
+                    spriteBee.setPosition(1920 + 150, 750);
+                }
+                beeSpeed = rand() % 200 + 100;
+            }
 
 
-        window.draw(spriteBackground);
-        window.draw(spriteCloud);
-        window.draw(spriteCloud2);
-        window.draw(spriteCloud3);
 
-        window.draw(spriteTree);
-        window.draw(spriteBee);
-        window.display();
+
+            // 그리기
+            window.clear();
+            window.draw(spriteBackground);
+            for (int i = 0; i < 3; i++)
+            {
+                window.draw(spriteCloud[i]);
+            }
+
+            window.draw(spriteTree);
+            window.draw(spriteBee);
+            window.display();
+        
     }
-
-    return 0;
-} //안녕하세요
+        
+ }
