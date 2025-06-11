4d4095e373b1265bc5b63c2fb97da6461ca35f2e
김주홍 <gicente1@nate.com>
2025-06-11 11:21:48 +0900

    25.06.11 plus2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,34 @@ int main()
     sf::Texture textureBackground;
     textureBackground.loadFromFile("graphics/background.png");
 
+    sf::Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    sf::Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    sf::Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
     sf::Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
+    sf::Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    sf::Sprite spriteCloud;
+    spriteCloud.setTexture(textureCloud);
+    sf::Sprite spriteCloud2;
+    spriteCloud2.setTexture(textureCloud);
+    sf::Sprite spriteCloud3;
+    spriteCloud3.setTexture(textureCloud);
+    sf::Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+
+
+
+    spriteTree.setPosition(1920 * 0.5f - textureTree.getSize().x *0.5f, 0);
+    spriteCloud2.setPosition(0, 0 + textureCloud.getSize().y);
+    spriteCloud3.setPosition(0, 0 + textureCloud.getSize().y * 2);
+    spriteBee.setPosition(0, 750);
+
+
 
     while (window.isOpen())
     {
@@ -23,6 +49,12 @@ int main()
 
 
         window.draw(spriteBackground);
+        window.draw(spriteCloud);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
+        window.draw(spriteTree);
+        window.draw(spriteBee);
         window.display();
     }
 
